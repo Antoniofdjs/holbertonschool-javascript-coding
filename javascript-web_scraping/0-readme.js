e93cf46d79b3aae543f4af3bfc8eb5fce464dc09
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-13 12:06:35 -0400

    Read a file with js using fs

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const filePath = process.argv[2];
+
+// Check if a file path was provided as an argument
+if (!filePath) {
+  console.error("Usage: node 0-readme.js <file-path>");
+  process.exit(1); // Exit with a non-zero status to indicate failure
+}
+
+// Read the content of the file
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
