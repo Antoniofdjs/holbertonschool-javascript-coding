080759411500580ef96e32e281165e83039d2e09
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-13 12:32:50 -0400

    Change error outputs

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+// Get the file path from command-line arguments
 const filePath = process.argv[2];
 
-// Check if a file path was provided as an argument
+// Check if a file path was provided
 if (!filePath) {
   console.error('Usage: node 0-readme.js <file-path>');
   process.exit(1); // Exit with a non-zero status to indicate failure
@@ -12,8 +13,22 @@ if (!filePath) {
 // Read the content of the file
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    // Handle file not found error (ENOENT)
+    if (err.code === 'ENOENT') {
+      // Create custom error object with desired format
+      const customError = new Error(
+        `ENOENT: no such file or directory, open '${filePath}'`
+      );
+      customError.code = 'ENOENT';
+      customError.errno = -2;
+      customError.syscall = 'open';
+      customError.path = filePath;
+
+      console.error(customError); // Output the custom error object
+    } else {
+      console.error(err); // Handle other types of errors
+    }
   } else {
-    console.log(data);
+    console.log(data); // Display file content if read successfully
   }
 });
