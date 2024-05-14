f0e118bb7802fbbc4a0143abbfb2d1a177202188
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-14 14:16:16 -0400

    Count tasks by a users from api request

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const completedUsers = {};
+
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1); // Exit with an error code
+  } else {
+    // get the body that has <p> tags and is not JSON...
+    const users = JSON.parse(response.body);
+    users.forEach((user) => {
+      // Get values for user id and task(true or false)
+      const userId = user.userId;
+      const taskDone = user.completed;
+      console.log(userId);
+      console.log(taskDone);
+      // If User exists and task is done, add +1 to value
+      if (userId in completedUsers && taskDone === true) {
+        completedUsers[`${userId}`] += 1;
+      } else {
+        if (taskDone === true) {
+          completedUsers[`${userId}`] = 1;
+        }
+      }
+    });
+    console.log(completedUsers);
+  }
+});
