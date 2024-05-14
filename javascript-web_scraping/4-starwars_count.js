358b6eb15e43cfca5d7538b13663fd2e92bb6ce0
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-14 10:17:50 -0400

    Iterate multiple movies and acces different values

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require("request");
+
+const characterId = "18"; // Wedge antilles
+
+const url = "https://swapi-api.hbtn.io/api/films";
+
+let count = 0;
+request(url, (error, response) => {
+  if (error) {
+    console.error("Error:", error.message);
+    process.exit(1); // Exit with an error code
+  } else {
+    // Get body from json
+    const data = JSON.parse(response.body);
+    const movies = data.results;
+    // Iterate every movie
+    movies.forEach((movie) => {
+      // Iterate every character on each movie
+      movie.characters.forEach((character) => {
+        // if character <https:link> has characterId
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
