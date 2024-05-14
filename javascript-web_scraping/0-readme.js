eb89167a40bae05619e23a04b6a1b7c63ce3733c
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-14 08:56:33 -0400

    ANother version test

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 // Get the file path from command-line arguments
 const filePath = process.argv[2];
 if (!filePath) {
-  console.log("Error: Please provide a file path as an argument.");
+  console.log('Error: Please provide a file path as an argument.');
   process.exit(1);
 }
 
-fs.readFile(filePath, "utf8", (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log({ error: err });
   } else {
