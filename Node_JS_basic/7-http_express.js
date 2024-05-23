ba6607e027e720766a60957b76d1be9e43e29198
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-23 07:41:13 -0400

    eslint fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
+const dataBase = args[0];
 const app = express();
 const port = 1245;
 
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
-    const students = await countStudents(DATABASE);
+    const students = await countStudents(dataBase);
     res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
     res.send(`${msg}${error.message}`);
